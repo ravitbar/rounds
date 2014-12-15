959a453118f1a198ed6d0137b8cb1632ac47fb54
Ravit Bar <ravitb@gmail.com>
2014-12-15 18:50:21 +0200

    added worker class

--- workArounds/models.py
+++ workArounds/models.py
@@ -8,16 +8,24 @@ from datetime import datetime
 def default_time(hour):
     now = datetime.now()
     start = now.replace(hour=hour, minute=0, second=0, microsecond=0)
-    return start if start > now else start + timedelta(days=1)  
+    return start if start > now else start + timedelta(days=1)
+
+class Worker(models.Model):
+    worker = models.ForeignKey('auth.User')
+    name   = models.CharField(max_length=200)
+
+    def setCalendar(currentMonth):
+
+    def getCalendar(currentMonth):
 
 class WorkDay(models.Model):
-    day    = models.ForeignKey('')
+    day    = models.DateField()
     worker = models.ForeignKey('auth.User')
-    start  = models.DateTimeField(default = default_time(10))
-    end    = models.DateTimeField(default = default_time(19))
+    start  = models.DateTimeField()
+    end    = models.DateTimeField()
 
 class MonthlyReport(models.Model):
-    month  = models.ForeignKey('')
+    month  = models.DateField()
     worker = models.ForeignKey('auth.User')
     total  = models.IntField()
     approvedDate = models.DateTimeField(blank=True, null=True)
